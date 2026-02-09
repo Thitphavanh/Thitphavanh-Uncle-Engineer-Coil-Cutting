9d212f668b9869590c6b386c6ba65f3fb607fddd
Thitphavanh <thitphavanh.psv@gmail.com>
2026-02-09 16:43:27 +0700

    feat: Integrate Flatpickr for enhanced datetime input in the coilout form.

--- coil/forms.py
+++ coil/forms.py
@@ -66,9 +66,9 @@ class CoilOutForm(forms.ModelForm):
         ]
         widgets = {
             'timestamp1': forms.DateTimeInput(attrs={
-                'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50',
-                'type': 'datetime-local'
-            }),
+                'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 datetimepicker',
+                'placeholder': 'YYYY-MM-DD HH:MM'
+            }, format='%Y-%m-%d %H:%M'),
             'user': forms.Select(attrs={
                 'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
             }),
